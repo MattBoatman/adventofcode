0a2b95240a5dc62c6f6bd95bcaa89b37c4a2fac0
Matt Boatman <matthew.boatman@capitalone.com>
2016-12-01 22:43:07 -0500

    advent day 1 solution part 1

---
+++ day1/adv1.js
@@ -0,0 +1,62 @@
+"use strict";
+day1();
+
+function getData(){
+    let fs = require('fs'), filename = process.argv[2];
+    let text = fs.readFileSync(filename, 'utf8');
+    return text.split(', ');
+}
+
+function day1() {
+    let contents = getData();
+    let horizontal = 0, vertical = 0;
+    let direction = 0;
+    let dir = ['up','down','right','left'];
+    let currentDir = dir[0];
+    contents.forEach(function(v) {
+        var n = parseInt(v.slice(1));
+        switch(currentDir) {
+                case 'up':
+                if (v[0] === 'R') {
+                    horizontal += n; 
+                    currentDir=dir[2];
+                } else {
+                    horizontal -= n;
+                    currentDir=dir[3];
+                } 
+                    break;
+                case 'down':
+                if (v[0] == 'L') {
+                    horizontal += n; 
+                    currentDir=dir[2];
+                } else {
+                    horizontal -= n;
+                    currentDir=dir[3];
+                }
+                    break;
+                case 'left': 
+                    if (v[0] == 'R') {
+                        vertical += n;
+                        currentDir=dir[0];
+                    } else {
+                        vertical -= n;
+                        currentDir=dir[1];
+                    }
+                    break;
+                case 'right': 
+                    if(v[0] === 'L') {
+                        vertical += n; 
+                        currentDir=dir[0];
+                    } else {
+                        vertical -= n;
+                        currentDir=dir[1];
+                    } 
+                    break;
+                default:
+                    break;
+        }
+    });
+
+console.log(Math.abs(horizontal)+Math.abs(vertical));
+ 
+};
